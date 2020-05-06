e2bb2974ddd20db47208eef9210b6d510fb55f31
femarleycode <femarleycode@gmail.com>
2020-05-06 13:32:56 +0100

    Added fields for implementing service test

--- backend/src/test/java/com/qa/service/NoteServiceUnitTest.java
+++ backend/src/test/java/com/qa/service/NoteServiceUnitTest.java
@@ -6,5 +6,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class NoteServiceUnitTest {
 
+    @InjectMocks
+    private NoteService service;
+
+    @Mock
+    private NotesRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private List<Note> noteList;
+
+    private Note_testNote;
+
+    private_long id = 1L;
+
+    private_Note testNoteWithId;
+
+    private_NoteDTO noteDTO(Note note){
+        return this.mapper.map(note, NoteDTO.class);
+    }
+
 
 }
