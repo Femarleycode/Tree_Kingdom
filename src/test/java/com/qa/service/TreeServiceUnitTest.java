457fc16f9102d9f05f24724c7d5f8e26a33953af
femarleycode <femarleycode@gmail.com>
2020-05-11 12:04:52 +0100

    update

--- src/test/java/com/qa/service/TreeServiceUnitTest.java
+++ src/test/java/com/qa/service/TreeServiceUnitTest.java
@@ -1,93 +1,93 @@
-package com.qa.service;
-
-import com.qa.domain.Tree;
-import com.qa.domain.Users;
-import com.qa.dto.TreeDTO;
-import com.qa.repo.TreeRepository;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.modelmapper.ModelMapper;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.mockito.Mockito.*;
-
-@RunWith(SpringRunner.class)
-public class TreeServiceUnitTest {
-
-    @InjectMocks
-    private TreeService service;
-
-    @Mock
-    private TreeRepository repository;
-
-    @Mock
-    private ModelMapper mapper;
-
-    private Set<Tree> treeSet;
-
-    private Tree testTree;
-
-    private final long treeId = 1L;
-
-    private Tree testTreeWithID;
-
-    private TreeDTO treeDTO;
-
-    private TreeDTO mapToDTO(Tree tree){
-        return this.mapper.map(tree, TreeDTO.class);
-    }
-
-    @Before
-    public void setUp(){
-
-        this.treeSet = new HashSet<>();
-        this.testTree = new Tree("oak", "fagales");
-        this.treeSet.add(testTree);
-        this.testTreeWithID = new Tree(testTree.getTreeName(), testTree.getOrderName());
-        this.testTreeWithID.setId(treeId);
-        this.treeDTO = this.mapToDTO(testTreeWithID);
-    }
-
-    @Test
-    public void getAllTreesTest(){
-        when(repository.findAll()).thenReturn(this.treeList);
-        when(this.mapper.map(testTreeWithID, TreeDTO.class)).thenReturn(treeDTO);
-        assertFalse("Service returned no Trees", this.service.readTrees().isEmpty());
-        verify(repository, times(1)).findAll();
-    }
-
-    @Test
-    public void createTreeTest(){
-        when(repository.save(testTree)).thenReturn(testTreeWithID);
-        when(this.mapper.map(testTreeWithID, TreeDTO.class)).thenReturn(treeDTO);
-        assertEquals(this.service.createTree(testTree), this.treeDTO);
-        verify(repository, times(1)).save(this.testTree);
-    }
-
-    @Test
-    public void findTreeByIdTest(){
-        when(this.repository.findById(treeId)).thenReturn(java.util.Optional.ofNullable(testTreeWithID));
-        when(this.mapper.map(testTreeWithID, TreeDTO.class)).thenReturn(treeDTO);
-        assertEquals(this.service.findTreeById(this.treeId), treeDTO);
-        verify(repository, times(1)).findById(treeId);
-    }
-
-    @Test
-    public void deleteTreeByExistingId(){
-        when(this.repository.existsById(treeId)).thenReturn(true, false);
-        assertFalse(service.deleteTree(treeId));
-        verify(repository, times(1)).deleteById(treeId);
-        verify(repository, times(2)).existsById(treeId);
-    }
+//package com.qa.service;
+//
+//import com.qa.domain.Tree;
+//import com.qa.domain.Users;
+//import com.qa.dto.TreeDTO;
+//import com.qa.repo.TreeRepository;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertFalse;
+//import static org.mockito.Mockito.*;
+//
+//@RunWith(SpringRunner.class)
+//public class TreeServiceUnitTest {
+//
+//    @InjectMocks
+//    private TreeService service;
+//
+//    @Mock
+//    private TreeRepository repository;
+//
+//    @Mock
+//    private ModelMapper mapper;
+//
+//    private Set<Tree> treeSet;
+//
+//    private Tree testTree;
+//
+//    private final long treeId = 1L;
+//
+//    private Tree testTreeWithID;
+//
+//    private TreeDTO treeDTO;
+//
+//    private TreeDTO mapToDTO(Tree tree){
+//        return this.mapper.map(tree, TreeDTO.class);
+//    }
+//
+//    @Before
+//    public void setUp(){
+//
+//        this.treeSet = new HashSet<>();
+//        this.testTree = new Tree("oak", "fagales");
+//        this.treeSet.add(testTree);
+//        this.testTreeWithID = new Tree(testTree.getTreeName(), testTree.getOrderName());
+//        this.testTreeWithID.setId(treeId);
+//        this.treeDTO = this.mapToDTO(testTreeWithID);
+//    }
+//
+//    @Test
+//    public void getAllTreesTest(){
+//        when(repository.findAll()).thenReturn(this.treeList);
+//        when(this.mapper.map(testTreeWithID, TreeDTO.class)).thenReturn(treeDTO);
+//        assertFalse("Service returned no Trees", this.service.readTrees().isEmpty());
+//        verify(repository, times(1)).findAll();
+//    }
+//
+//    @Test
+//    public void createTreeTest(){
+//        when(repository.save(testTree)).thenReturn(testTreeWithID);
+//        when(this.mapper.map(testTreeWithID, TreeDTO.class)).thenReturn(treeDTO);
+//        assertEquals(this.service.createTree(testTree), this.treeDTO);
+//        verify(repository, times(1)).save(this.testTree);
+//    }
+//
+//    @Test
+//    public void findTreeByIdTest(){
+//        when(this.repository.findById(treeId)).thenReturn(java.util.Optional.ofNullable(testTreeWithID));
+//        when(this.mapper.map(testTreeWithID, TreeDTO.class)).thenReturn(treeDTO);
+//        assertEquals(this.service.findTreeById(this.treeId), treeDTO);
+//        verify(repository, times(1)).findById(treeId);
+//    }
+//
+//    @Test
+//    public void deleteTreeByExistingId(){
+//        when(this.repository.existsById(treeId)).thenReturn(true, false);
+//        assertFalse(service.deleteTree(treeId));
+//        verify(repository, times(1)).deleteById(treeId);
+//        verify(repository, times(2)).existsById(treeId);
+//    }
 
 
 
@@ -119,4 +119,4 @@ public class TreeServiceUnitTest {
 //        verify(this.repository, times(1)).save(updateTree);
 //    }
 
-}
+//}
